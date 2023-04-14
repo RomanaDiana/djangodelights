c6a731c97a42dec4c6952596c364e4c808b03d78
RomanaDiana <peshokhardr@gmail.com>
2023-04-14 23:47:04 +0300

    Add models to inventory app

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,27 @@
+import datetime
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=20)
+    quantity = models.IntegerField(default=0)
+    unit = models.CharField(max_length=10)
+    unit_price = models.IntegerField()
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=30)
+    price = models.IntegerField()
+    image_url = models.URLField(max_length=200, null=True, blank=True)
+    recipe_url = models.URLField(max_length=200, null=True, blank=True)
+
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=datetime.date)
