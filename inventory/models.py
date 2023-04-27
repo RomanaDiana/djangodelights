72e3aa0349832c610914949d6c951e09ccb69b18
RomanaDiana <peshokhardr@gmail.com>
2023-04-27 14:17:18 +0300

    Add __str__() method to all models available

--- inventory/models.py
+++ inventory/models.py
@@ -8,12 +8,24 @@ class Ingredient(models.Model):
     unit = models.CharField(max_length=10)
     unit_price = models.FloatField()
 
+    def __str__(self):
+        return f"""
+        name = {self.name};
+        quantity = {self.quantity};
+        unit = {self.unit};
+        unit_price = {self.unit_price};
+        """
+
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=30)
     price = models.FloatField()
     image_url = models.URLField(max_length=200, null=True, blank=True)
     recipe_url = models.URLField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f"title = {self.title}; price = {self.price}"
+
 
     def available(self):
         return all(
@@ -26,6 +38,9 @@ class RecipeRequirement(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     quantity = models.FloatField()
 
+    def __str__(self):
+        return f"menu_item = [{self.menu_item.__str__()}]; ingredient = {self.ingredient.name}; quantity = {self.quantity}"
+
     def enough(self):
         return self.quantity <= self.ingredient.quantity
 
@@ -33,3 +48,6 @@ class RecipeRequirement(models.Model):
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(default=datetime.date)
+
+    def __str__(self):
+        return f"menu_item=[{self.menu_item.__str__()}]; timestamp = {self.timestamp}"
